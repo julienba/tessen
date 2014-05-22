852f45a4948151ce992cfd4508fdd3bd566b7003
Jake Davis <jake.davis5989@gmail.com>
2014-05-22 05:40:51 +0000

    First commit -- scaffolding.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tessen
+Sensu API client written in Clojure.
+
+## Usage
+``` clojure
+```
+
+## Author/License
+Jake Davis <jake@simple.com>
+
+Apache License, Version 2.0
---
+++ project.clj
@@ -0,0 +1,15 @@
+(def VERSION (.trim (slurp "VERSION")))
+
+(defproject tessen VERSION
+  :description "Sensu API client in Clojure."
+  :url "http://github.com/jakedavis/tessen"
+  :license "Apache License, Version 2.0"
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/algo.generic "0.1.2"]
+                 [cheshire "5.3.1"]
+                 [clj-http "0.9.1"]]
+  :main com.simple.tessen.core
+  :min-lein-version "2.0.0"
+  :global-vars {*warn-on-reflection* true})
