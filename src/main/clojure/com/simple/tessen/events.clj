bbd14c6369fdd2d77c3f1de2a4c636a629aeaad1
Jake Davis <jake.davis5989@gmail.com>
2014-05-28 20:15:23 +0000

    Add core namespace and improve events.
    This adds a core namespace for dispatching high level endpoint commands to
    other namespaces. It then applies the args given to another dispatch function
    in each namspace.
    
    Adds an info and health function to core which hit the corresponding endpoints
    in the Sensu API.
    
    Also semi-settled on a format for passing the host around. Every function
    accepts a host parameter, but it's the first in core/dispatch, meaning you can
    easily partially apply it and then not have to worry about it again.

--- src/main/clojure/com/simple/tessen/events.clj
+++ src/main/clojure/com/simple/tessen/events.clj
@@ -1,6 +1,5 @@
 (ns com.simple.tessen.events
-  (:require [cheshire.core :as json]
-            [clj-http.client :as http]))
+  (:require [clj-http.client :as http]))
 
 (defn all
   "Returns all events."
@@ -10,13 +9,25 @@
 (defn for-client
   "Returns all events for the Sensu client CLIENT, filtering additionally
    for optional check name CHECK."
-  [& {:keys [host port client check]
-      :or {port 4567 check "/"}}]
-  (let [url (str host ":" port "/events/" client check)] 
+  [host client & {:keys [check]}]
+  (let [path (str client "/" check)
+        url (str host path)] 
     (http/get url)))
 
 (defn resolve-event
   "Resolves the Event categorized by the host name CLIENT and with check name
    CHECK."
   [host client check]
-  (http/get ))
+  (let [url (str host "/resolve")
+        payload (json/generate-string {:client client :check check})]
+    (http/post url {:body payload :content-type :json})))
+
+(defn dispatch
+  "Dispatches the keyword CMD to be run as an associated function call, with
+   additional arguments ARGS."
+  [host cmd & args]
+  (case cmd
+    "all" (apply all host args)
+    "for-client" (apply for-client host args)
+    "resolve" (apply resolve-event host args)
+    :default (println (format "Unknown command `%s'" cmd))))
